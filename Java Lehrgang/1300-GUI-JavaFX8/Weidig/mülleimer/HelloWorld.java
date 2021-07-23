0c07ce2e02a938b6788b6eb2446d912fe061e309
TheHeadless <adrianweidig@gmx.net>
2021-07-24 00:02:42 +0200

    "Aktualisierung"

---
+++ Java Lehrgang/1300-GUI-JavaFX8/Weidig/mülleimer/HelloWorld.java
@@ -0,0 +1,21 @@
+package mülleimer;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String frank = "Frank";
+		int a = 1000000000;
+		long b = 10000000000L;
+		boolean wahr = true;
+		double c = 2.0;
+		float d = 10000000000.0F;
+		
+		
+		System.out.println(frank);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(wahr);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
